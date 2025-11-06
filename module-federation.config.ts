fcde22c072134bfdf014dd93eb22c37c5add35bb
NANDRADE-BMSoluciones <nandrade@bmsoluciones.cl>
2025-11-06 16:56:12 -0300

    feat: update module federation config to use production URLs for colorPicker and colorList

--- module-federation.config.ts
+++ module-federation.config.ts
@@ -3,13 +3,9 @@ export const mfConfig = {
   exposes: {},
   remotes: {
     colorPicker:
-      process.env.NODE_ENV === "development"
-        ? "mf_colorpicker@http://localhost:3001/remoteEntry.js"
-        : "mf_colorpicker@https://mf-colorpicker-nar.netlify.app/remoteEntry.js",
+      "mf_colorpicker@https://mf-colorpicker-nar.netlify.app/remoteEntry.js",
     colorList:
-      process.env.NODE_ENV === "development"
-        ? "mf_colorlist@http://localhost:3002/remoteEntry.js"
-        : "mf_colorlist@https://mf-colorlist-nar.netlify.app/remoteEntry.js",
+      "mf_colorlist@https://mf-colorlist-nar.netlify.app/remoteEntry.js",
   },
   shared: {
     react: {
